405a3122f4907298f2f2adad2f048e4cfe8eadb8
digiTALker <d_talker@mail.ru>
2025-09-14 17:25:48 +0500

    Replace icon binary with base64

--- plugin.php
+++ plugin.php
@@ -4,6 +4,7 @@ return array(
     'name'               => 'AnonGuard — деперсонификация заказов и контактов',
     'vendor'             => 'incyber',
     'version'            => '0.3.1',
+    'img'                => 'img/icon.png',
     'shop_version_from'  => '8.0',
     'shop_version_to'    => '',
     'description'        => 'Обезличивает персональные данные в заказах и контактах старше заданного срока.',
